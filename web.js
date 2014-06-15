8a3987543005ea145966c21e30bd0d1831c74ceb
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-06-15 23:04:32 +0200

    LinkedIn using passport

--- web.js
+++ web.js
@@ -1,118 +1,57 @@
 var express = require('express')
   , http = require('http')
   , passport = require('passport')
-  , util = require('util')
-  , LinkedInStrategy = require('passport-linkedin').Strategy;
-
+  , OAuth2Strategy = require('passport-oauth').OAuth2Strategy
+  , request = require("request");
+ 
 var app = express();
 
 // configure Express
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.logger());
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'keyboard cat' }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(app.router);
-app.use(express.static(__dirname + '/public'));
 
 var LINKEDIN_API_KEY = "77o5utzfrab1wz";
 var LINKEDIN_SECRET_KEY = "S1NTOHbq5juFqFFj";
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete LinkedIn profile is
-//   serialized and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+var LINKEDIN_CONSUMER_KEY = "f7037e9e-2db3-48be-87cf-0f175b5fc391";
+var LINKEDIN_CONSUMER_SECRET = "72ce6c51-4402-4232-b7ca-ae65c3bca933";
+var LINKEDIN_CALLBACK_URL = "https://sleepy-lowlands-2774.herokuapp.com/auth/linkedin/callback";
+//var LINKEDIN_CALLBACK_URL = "http://localhost:5000/auth/linkedin/callback";
 
 
-// Use the LinkedInStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a token, tokenSecret, and LinkedIn profile), and
-//   invoke a callback with a user object.
-passport.use(new LinkedInStrategy({
-    consumerKey: "f7037e9e-2db3-48be-87cf-0f175b5fc391",
-    consumerSecret: "72ce6c51-4402-4232-b7ca-ae65c3bca933",
-    callbackURL: "http://sleepy-lowlands-2774.herokuapp.com/auth/linkedin/callback"
+passport.use('linkedin', new OAuth2Strategy({
+    authorizationURL: 'https://www.linkedin.com/uas/oauth2/authorization?response_type=code&state=BFEDDCAF45453sdffef424',
+    tokenURL: 'https://www.linkedin.com/uas/oauth2/accessToken',
+    clientID: LINKEDIN_API_KEY,
+    clientSecret: LINKEDIN_SECRET_KEY,
+    callbackURL: LINKEDIN_CALLBACK_URL
   },
-  function(token, tokenSecret, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      // To keep the example simple, the user's LinkedIn profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the LinkedIn account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
+  function(accessToken, refreshToken, profile, done) {
+    request({
+	  uri: "http://api.linkedin.com/v1/people/~/connections",
+	  method: "GET",
+	  timeout: 10000,
+	  followRedirect: true,
+	  maxRedirects: 10,
+	  headers: { 'x-li-format': 'json' },
+	  qs: { oauth2_access_token: accessToken }
+	}, function(error, response, body) {
+	  console.log(body);
+      done(error, null);
+	});
   }
 ));
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
-});
-
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
-});
-
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user });
-});
-
-// GET /auth/linkedin
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in LinkedIn authentication will involve
-//   redirecting the user to linkedin.com.  After authorization, LinkedIn will
-//   redirect the user back to this application at /auth/linkedin/callback
 app.get('/auth/linkedin',
   passport.authenticate('linkedin'),
   function(req, res){
-    // The request will be redirected to LinkedIn for authentication, so this
-    // function will not be called.
   });
 
-// GET /auth/linkedin/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get('/auth/linkedin/callback',
   passport.authenticate('linkedin', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
