1e842a2e22d61376683674912f87ca619e1034a9
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-06-26 09:55:35 +0200

    Allows to use local files to blend

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,10 +6,42 @@ for(var i=1; i<24; i++) {
 	images.push('./images/jap/jap' + i + '.png');
 }
 
-smoisheleAnalyser.getFaceFeatures(images, function(detectedFaces){
+function startBlend(images) {
 	'use strict';
-	smoisheleBlender.blend(detectedFaces, function(image) {
-		window.open(image, '', '_blank');
+	smoisheleAnalyser.getFaceFeatures(images, function(detectedFaces){
+		smoisheleBlender.blend(detectedFaces, function(image) {
+			window.open(image, '', '_blank');
+		});
+		
 	});
-	
-});
+}
+
+function handleFileSelect(evt) {
+	'use strict';
+	// filter the input for image files
+	var files = evt.target.files;
+	var fileList = [];
+	for (var i = 0;i < files.length;i++) {
+		if (!files[i].type.match('image.*')) {
+			continue;
+		}
+		fileList.push(files[i]);
+	}
+
+	// load all files into file urls
+	var imagesUrls = [];
+	fileList.forEach(function(file) {
+		var reader = new FileReader();
+		reader.onload = (function() {
+			return function(e) {
+				imagesUrls.push(e.target.result);
+				if (imagesUrls.length === files.length) {
+					startBlend(imagesUrls);
+				}
+			};
+		})(file);
+		reader.readAsDataURL(file);
+	});
+
+}
+document.getElementById('file-upload-button').addEventListener('change', handleFileSelect, false);
