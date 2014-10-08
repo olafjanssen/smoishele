b6ee37953c2ca4e34717ea6e71b3d216d3023d22
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-10-09 00:04:52 +0200

    Added H1 and H2 tags using the smoishele font instead of svg images. Show introduction animation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,16 @@
 /*
     Main code
  */
+'use strict';
+
+setTimeout(function(){
+    document.body.classList.add('show-intro');
+}, 1000);
+
+setTimeout(function(){
+    document.body.classList.remove('during-intro');
+}, 8000);
+
+setTimeout(function(){
+    document.body.classList.remove('show-intro');
+}, 5000);
