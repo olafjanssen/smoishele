6ac9f5e5fa40c1bd17ae1f18dc7bf87669531c84
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-10-09 22:52:30 +0200

    Added information panel with GitHub fork button and PayPal donate button and proper library credits.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,3 +14,9 @@ setTimeout(function(){
 setTimeout(function(){
     document.body.classList.remove('show-intro');
 }, 5000);
+
+$(function(){
+    $('h1').click(function(){
+        document.body.classList.toggle('show-info');
+    });
+});
