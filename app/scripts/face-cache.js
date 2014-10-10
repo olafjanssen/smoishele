1710ca6ef3c9ffc69dd05ff5a6551d1b0ef61bb0
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-10-10 23:11:49 +0200

    Added advanced panel for downloading and uploading the in-memory database.

--- app/scripts/face-cache.js
+++ app/scripts/face-cache.js
@@ -25,10 +25,25 @@ var smoisheleFaceCache = (function() {
 		faces = {};
 	}
 
+    function handleFileSelect(evt) {
+        var file = evt.target.files[0];
+
+        if (file && file.type.match('application.json')) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var contents = e.target.result;
+                faces = JSON.parse(contents);
+            };
+            reader.readAsText(file);
+        }
+    }
+
 	document.getElementById('dbase-download').addEventListener('click', function(){
-		var newWindow = window.open('', '_blank');
-		newWindow.document.write(JSON.stringify(faces));
+        var data = new Blob([JSON.stringify(faces)], {type: 'application/json'});
+        document.getElementById('dbase-download').href = URL.createObjectURL(data);
 	}, false);
+
+    document.getElementById('dbase-upload').addEventListener('change', handleFileSelect, false);
 
 	return {addFace: addFace, getFaces: getFaces, reset: reset};
 })();
