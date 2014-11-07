54f5787fab2d1bc4f2e10e6b2f0c9fae2deb04fa
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-11-07 23:55:31 +0100

    Add Rembrandt demo.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,15 +5,37 @@
 
 setTimeout(function(){
     document.body.classList.add('show-intro');
-}, 1000);
+}, 5000);
 
 setTimeout(function(){
     document.body.classList.remove('during-intro');
-}, 8000);
+}, 16000);
 
 setTimeout(function(){
     document.body.classList.remove('show-intro');
+}, 10000);
+
+
+setTimeout(function(){
+    document.body.classList.add('demo1');
+}, 1000);
+
+setTimeout(function(){
+    document.body.classList.add('demo2');
+    document.body.classList.remove('demo1');
 }, 5000);
+
+setTimeout(function(){
+    document.body.classList.add('demo3');
+    document.body.classList.remove('demo2');
+}, 7000);
+
+setTimeout(function(){
+    document.body.classList.remove('demo3');
+}, 13000);
+
+
+
 
 $(function(){
     $('h1').click(function(){
